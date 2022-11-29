37ae917f21669ab68a92b5fe57ea8353922e5474
Will Morano <wfm224@lehigh.edu>
2022-11-29 09:49:11 -0500

    a

--- javascript/about.js
+++ javascript/about.js
@@ -20,7 +20,10 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     var name = e.target.querySelector('#fname').value
     box.innerText = name ? `Thanks ${name}, for the submission!` : `Thanks!, for the submission!`
+    box.style.fontFamily = 'Raleway'
+    box.style.fontSize = '32px'
     box.style.display = 'block'
+    box.style.padding = '10px'
     setTimeout(() => {
         box.remove()
     }, 1000)
